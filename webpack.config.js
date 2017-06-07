3ade020139ce0f807c60c214790f6545c5d8cf09
Matthew Finerty <evilarceus@gmail.com>
2017-06-07 00:49:25 -0700

    Add Webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: ['./app/index.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8080/'
+    },
+    devServer: {
+        contentBase: './app',
+        publicPath: 'http://localhost:8080/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './app/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            include: path.join(__dirname, 'app'),
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin()
+    ]
+};
