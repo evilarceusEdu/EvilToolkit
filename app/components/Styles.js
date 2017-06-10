8a9f2499cac4957070af273c58f50858f63d5697
Matthew Finerty <evilarceus@gmail.com>
2017-06-10 00:47:59 -0700

    Added component for styling

---
+++ app/components/Styles.js
@@ -0,0 +1,14 @@
+module.exports = {
+    contentStyle: {
+        /* WORST HACK EVER - DONUT STEEL ( ͡° ͜ʖ ͡°) */
+        marginLeft: '256px',
+    },
+
+    bottomNavStyle: {
+        /* Ok, this hack is even worse, but again, DONUT STEEL ( ͡° ͜ʖ ͡°) */
+        position: "fixed",
+        bottom: "0px",
+        right: "10%",
+        left: "10%"
+    }
+};
