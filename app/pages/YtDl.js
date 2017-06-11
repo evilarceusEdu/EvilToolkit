93320f41ad0d262b0d3ec877892e770caad8f0bd
Matthew Finerty <evilarceus@gmail.com>
2017-06-11 12:32:09 -0700

    Added embeded video preview to YtDl page

--- app/pages/YtDl.js
+++ app/pages/YtDl.js
@@ -1,16 +1,71 @@
 import React from "react";
-import {MuiThemeProvider} from "material-ui";
+import {Dialog, FlatButton, MuiThemeProvider, RaisedButton, TextField} from "material-ui";
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import * as Config from "../Config";
 import {BottomNav, MainDrawer, Styles, TitleBar} from '../components'
 
-const contentStyle = {
-    /* WORST HACK EVER - DONUT STEEL ( ͡° ͜ʖ ͡°) */
-    marginLeft: '256px'
-};
+class Embed extends React.Component {
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render() {
+        return(
+            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + this.props.videoId} allowfullscreen></iframe>
+        )
+    }
+}
 
 export default class YtDl extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showEmbed: false,
+            inputValue: "",
+            youtubeVideoId: "",
+            dialogOpen: false
+        };
+    }
+
+    getEmbededVideo = () => {
+        if (this.state.inputValue.includes("v=")) {
+            this.setState({youtubeVideoId: this.state.inputValue.substring(this.state.inputValue.indexOf("v=")).replace("v=", "")});
+            if (this.state.inputValue.includes("&")) {
+                this.setState({
+                    youtubeVideoId: this.state.inputValue.substring(this.state.inputValue.indexOf("v="), this.state.inputValue.indexOf("&")).replace("v=", "")
+                })
+            }
+            this.showEmbededVideo();
+        } else this.handleDialogOpen();
+
+    };
+
+    showEmbededVideo = () => {
+        this.setState({showEmbed: true});
+    };
+
+    updateInputValue = (evt) => {
+        this.setState({inputValue: evt.target.value});
+    };
+
+    handleDialogOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleDialogClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
+        const actions = [
+            <FlatButton
+                label="Ok"
+                primary={true}
+                onTouchTap={this.handleDialogClose}
+            />
+        ];
+
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(Config.theme.ytdl)}>
                 <div>
@@ -19,6 +74,27 @@ export default class YtDl extends React.Component {
                         <TitleBar title="YouTubeDL" icon="back" />
                         <BottomNav>
                             <div className="tool">
+                                <div className="video" style={{textAlign: "center", margin: "auto"}}>
+                                    { this.state.showEmbed ? <Embed videoId={this.state.youtubeVideoId} /> : null}
+                                </div>
+                                <div className="centerAlign" style={{textAlign: "center", margin: "auto",}}>
+                                    <TextField
+                                        hintText="YouTube URL"
+                                        style={{width: "50%"}}
+                                        value={this.state.inputValue}
+                                        onChange={this.updateInputValue}
+                                    />
+                                    <RaisedButton label="Submit" primary={true} onClick={this.getEmbededVideo}/>
+                                </div>
+                                <Dialog
+                                    title="YouTubeDL"
+                                    actions={actions}
+                                    modal={false}
+                                    open={this.state.open}
+                                    onRequestClose={this.handleDialogClose}
+                                >
+                                    Please enter a valid YouTube URL (ex. https://www.youtube.com/watch?v=[videoID])
+                                </Dialog>
 
                             </div>
                             <div className="source">
